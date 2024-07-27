b64b2f181fe547d6d9d5ade58c90c382fc3040eb
sohanr <mizan.rahman66d@gmail.com>
2024-07-27 14:33:48 +0600

    app

---
+++ src/main/java/dev/sohan/spring_security/SpringSecurityApplication.java
@@ -0,0 +1,35 @@
+package dev.sohan.spring_security;
+
+import dev.sohan.spring_security.entities.Role;
+import dev.sohan.spring_security.entities.User;
+import dev.sohan.spring_security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class SpringSecurityApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+	public void run(String... args) {
+		User adminAccount = userRepository.findByRole(Role.ADMIN);
+		if(null == adminAccount){
+			User user = new User();
+
+			user.setEmail("admin@gmail.com");
+			user.setFirstName("admin");
+			user.setLastName("admin");
+			user.setRole(Role.ADMIN);
+			user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+			userRepository.save(user);
+		}
+	}
+}
